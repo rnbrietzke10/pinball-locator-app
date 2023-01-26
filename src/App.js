2f95336f10e31b29d11d242718b02de3d30051e8
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-01-26 07:38:11 -0600

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+// Photo by <a href="https://unsplash.com/@kylejeffreys?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kyle Johnson</a> on <a href="https://unsplash.com/photos/7dy20Hc1wWE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Locate A Pinball Machine Near You!</h1>
       </header>
     </div>
   );
